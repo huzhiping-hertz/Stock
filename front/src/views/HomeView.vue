6482d5d7631197a4cfb819b537bc93f08afd683e
huzhiping-hertz <huzhiping001@126.com>
2023-04-28 13:59:23 +0800

    update

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -202,7 +202,7 @@ function onSearch() {
                         icon: 'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
                         onclick: function () {
 
-                            var mdata = dataclose.slice(pts[0], pts[pts.length])
+                            var mdata = dataclose.slice(pts[0], pts[pts.length-1])
                             var min = Math.min(...mdata)
                             var max = Math.max(...mdata)
                             var normal_data = mdata.map(x => ((x - min) / (max - min)).toFixed(2))
